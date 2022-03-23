7a2d531aaa108fa96f0734ff91d4b15c9d6f995c
Mohit Sanghai <sanghai.m@northeastern.edu>
2022-03-23 16:42:25 -0400

    Adding First version of the Tracker

---
+++ src/main/java/com/mms/covidtracker/controllers/HomeController.java
@@ -0,0 +1,29 @@
+package com.mms.covidtracker.controllers;
+
+import com.mms.covidtracker.models.LocationStats;
+import com.mms.covidtracker.services.CoronaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CoronaVirusDataService coronaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+}
